e8da8aad6ccd5112d2da338a885d625a88ef1d08
hakaryzhang <hakaryzhang36@163.com>
2022-09-23 22:47:28 +0800

    add new solutions

---
+++ workplace/src/com/hakaryzhang/solution/Solution.java
@@ -0,0 +1,15 @@
+package com.hakaryzhang.solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+
+
+}
