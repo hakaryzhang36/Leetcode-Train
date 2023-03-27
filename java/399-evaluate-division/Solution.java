e437af6f6ff5362f73cc1c946f1db18d2efa4813
hakaryzhang <hakaryzhang36@163.com>
2023-03-27 17:24:54 +0800

    Update: add new solutions

---
+++ java/399-evaluate-division/Solution.java
@@ -0,0 +1,7 @@
+// Leetcode 399-evaluate-division
+// mark
+class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+    }
+}
--- java/406-queue-reconstruction-by-height/Solution.java
+++ java/406-queue-reconstruction-by-height/Solution.java
@@ -1,24 +1,45 @@
 // Leetcode 406-queue-reconstruction-by-height
 // mark
+// done
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
-        for (int i = 0; i < people.length; i++) {
-            for (int j = i + 1; j < people.length; j++) {
-                if (people[i][0] > people[j][0]) {
-                    int[] temp = people[i];
-                    people[i] = people[j];
-                    people[j] = temp;
+        Arrays.sort(people, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[0] != b[0]) {
+                    return a[0] - b[0];
                 }
-                else if (people[i][0] == people[j][0] && people[i][1] > people[j][1]) {
-                    int[] temp = people[i];
-                    people[i] = people[j];
-                    people[j] = temp;
+                else {
+                    return a[1] - b[1];
                 }
             }
-        }
+        });
 
         for (int i = people.length-1; i >= 0; i--) {
-            int 
+            int e = 0;  // 前面有几个相等的
+            int m = i-1;
+            while (m >= 0 && people[i][0] == people[m][0]) {
+                m--;
+                e++;
+            }
+            while (i > m) {
+                sub(i, e, people);
+                i--;
+                e--;
+            }
+            i = m+1;
+        }
+
+        return people;
+    }
+
+    public void sub(int m, int e, int[][] people) {
+        while (people[m][1] > e) {
+            int[] temp = people[m];
+            people[m] = people[m+1];
+            people[m+1] = temp;
+            e++;
+            m++;
         }
     }
 }
